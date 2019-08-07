c0e80aff86bf3ff987c9d7497a0b6a1bffc7f186
Jeff <jefftully@gmail.com>
2019-08-07 11:11:42 -0400

    wth?

--- TodoList.js
+++ TodoList.js
@@ -1,20 +1,12 @@
 import React, { Component } from "react";
 class TodoList extends Component {
-  componentDidUpdate() {
-    this.props.inputElement.current.focus();
-  }
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form onSubmit={this.props.addItem}>
-            <input
-              placeholder="Task"
-              ref={this.props.inputElement}
-              value={this.props.currentItem.text}
-              onChange={this.props.handleInput}
-            />
-            <button type="submit"> Add Task </button>
+          <form>
+            <input placeholder="enter task" />
+            <button type="submit">add</button>
           </form>
         </div>
       </div>
